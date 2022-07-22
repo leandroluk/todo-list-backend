8fdaba6006cab82bb1f5a988adf2ededef76e5a5
Leandro Santiago Gomes <leandroluk@gmail.com>
2022-07-22 20:36:51 -0300

    removendo console.log

--- tests/app/models/todos.model.spec.ts
+++ tests/app/models/todos.model.spec.ts
@@ -14,72 +14,72 @@ describe('app/models/todos.model', () => {
   describe('get', () => {
     it('should throw if TodosDAO.findOne throws', () => {
       sinon.stub(TodosDAO, 'findOne').rejects()
-      expect(todosModel.get(1)).to.eventually.be.rejected
+      return expect(todosModel.get(1)).to.eventually.be.rejected
     })
 
     it('should return result', () => {
       sinon.stub(TodosDAO, 'findOne').resolves({ id: 1 } as unknown as Model<Todo>)
-      expect(todosModel.get(1)).to.eventually.deep.equal({ id: 1 })
+      return expect(todosModel.get(1)).to.eventually.deep.equal({ id: 1 })
     })
   })
 
   describe('exists', () => {
     it('should throw if TodosDAO.count throws', () => {
       sinon.stub(TodosDAO, 'count').rejects()
-      expect(todosModel.exists(1)).to.eventually.be.rejected
+      return expect(todosModel.exists(1)).to.eventually.be.rejected
     })
 
     it('should return result', () => {
       sinon.stub(TodosDAO, 'count').resolves(1)
-      expect(todosModel.exists(1)).to.eventually.be.equal(1)
+      return expect(todosModel.exists(1)).to.eventually.be.true
     })
   })
 
   describe('edit', () => {
     it('should throw if TodosDAO.update throws', () => {
       sinon.stub(TodosDAO, 'update').rejects()
-      expect(todosModel.edit(1, {})).to.eventually.be.rejected
+      return expect(todosModel.edit(1, {})).to.eventually.be.rejected
     })
 
     it('should return result', () => {
       sinon.stub(TodosDAO, 'update').resolves()
-      expect(todosModel.edit(1, {})).to.eventually.be.undefined
+      return expect(todosModel.edit(1, {})).to.eventually.be.undefined
     })
   })
 
   describe('remove', () => {
     it('should throw if TodosDAO.destroy throws', () => {
       sinon.stub(TodosDAO, 'destroy').rejects()
-      expect(todosModel.remove(1)).to.eventually.be.rejected
+      return expect(todosModel.remove(1)).to.eventually.be.rejected
     })
 
     it('should return result', () => {
       sinon.stub(TodosDAO, 'destroy').resolves()
-      expect(todosModel.remove(1)).to.eventually.be.undefined
+      return expect(todosModel.remove(1)).to.eventually.be.undefined
     })
   })
 
   describe('add', () => {
     it('should throw if TodosDAO.create throws', () => {
       sinon.stub(TodosDAO, 'create').rejects()
-      expect(todosModel.add({} as AddTodo)).to.eventually.be.rejected
+      return expect(todosModel.add({} as AddTodo)).to.eventually.be.rejected
     })
 
     it('should return result', () => {
       sinon.stub(TodosDAO, 'create').resolves()
-      expect(todosModel.add({} as AddTodo)).to.eventually.be.undefined
+      return expect(todosModel.add({} as AddTodo)).to.eventually.be.undefined
     })
   })
 
   describe('list', () => {
     it('should throw if TodosDAO.findAll throws', () => {
       sinon.stub(TodosDAO, 'findAll').rejects()
-      expect(todosModel.list()).to.eventually.be.rejected
+      return expect(todosModel.list()).to.eventually.be.rejected
     })
 
     it('should return result', () => {
       sinon.stub(TodosDAO, 'findAll').resolves([{ id: 1 }] as unknown as Model<Todo>[])
-      expect(todosModel.list()).to.eventually.deep.equal([{ id: 1 }])
+      return expect(todosModel.list()).to.eventually.deep.equal([{ id: 1 }])
     })
   })
 })
--- tests/app/services/todos.service.spec.ts
+++ tests/app/services/todos.service.spec.ts
@@ -12,89 +12,89 @@ describe('app/services/todos.service', () => {
   describe('get', () => {
     it('should throw if todosModel.get throws', () => {
       sinon.stub(todosModel, 'get').rejects()
-      expect(todosService.get(1)).to.eventually.be.rejected
+      return expect(todosService.get(1)).to.eventually.be.rejected
     })
 
     it('should throw if todosModel.get return empty', () => {
       sinon.stub(todosModel, 'get').resolves()
-      expect(todosService.get(1)).to.eventually.be.rejected
+      return expect(todosService.get(1)).to.eventually.be.rejected
     })
 
     it('should return result', () => {
       sinon.stub(todosModel, 'get').resolves({ id: 1 } as Todo)
-      expect(todosService.get(1)).to.eventually.deep.equal({ id: 1 })
+      return expect(todosService.get(1)).to.eventually.deep.equal({ id: 1 })
     })
   })
 
   describe('edit', () => {
     it('should throw if todosModel.exists throw', () => {
       sinon.stub(todosModel, 'exists').rejects()
-      expect(todosService.edit(1, {})).to.eventually.be.rejected
+      return expect(todosService.edit(1, {})).to.eventually.be.rejected
     })
 
     it('should throw if todosModel.exists return false', () => {
       sinon.stub(todosModel, 'exists').resolves(false)
-      expect(todosService.edit(1, {})).to.eventually.be.rejected
+      return expect(todosService.edit(1, {})).to.eventually.be.rejected
     })
 
     it('should throw if todosModel.edit throws', () => {
       sinon.stub(todosModel, 'exists').resolves(true)
       sinon.stub(todosModel, 'edit').rejects()
-      expect(todosService.edit(1, {})).to.eventually.be.rejected
+      return expect(todosService.edit(1, {})).to.eventually.be.rejected
     })
 
     it('should return result', () => {
       sinon.stub(todosModel, 'exists').resolves(true)
       sinon.stub(todosModel, 'edit').resolves()
-      expect(todosService.edit(1, {})).to.eventually.be.undefined
+      return expect(todosService.edit(1, {})).to.eventually.be.undefined
     })
   })
 
   describe('remove', () => {
     it('should throw if todosModel.exists throw', () => {
       sinon.stub(todosModel, 'exists').rejects()
-      expect(todosService.remove(1)).to.eventually.be.rejected
+      return expect(todosService.remove(1)).to.eventually.be.rejected
     })
 
     it('should throw if todosModel.exists return false', () => {
       sinon.stub(todosModel, 'exists').resolves(false)
-      expect(todosService.remove(1)).to.eventually.be.rejected
+      return expect(todosService.remove(1)).to.eventually.be.rejected
     })
 
     it('should throw if todosModel.remove throws', () => {
       sinon.stub(todosModel, 'exists').resolves(true)
       sinon.stub(todosModel, 'remove').rejects()
-      expect(todosService.remove(1)).to.eventually.be.rejected
+      return expect(todosService.remove(1)).to.eventually.be.rejected
     })
 
     it('should return result', () => {
       sinon.stub(todosModel, 'exists').resolves(true)
       sinon.stub(todosModel, 'remove').resolves()
-      expect(todosService.remove(1)).to.eventually.be.undefined
+      return expect(todosService.remove(1)).to.eventually.be.undefined
     })
   })
 
   describe('add', () => {
     it('should throw if todosModel.add throws', () => {
       sinon.stub(todosModel, 'add').rejects()
-      expect(todosService.add({} as AddTodo)).to.eventually.be.rejected
+      return expect(todosService.add({} as AddTodo)).to.eventually.be.rejected
     })
 
     it('should return result', () => {
       sinon.stub(todosModel, 'add').resolves(1)
-      expect(todosService.add({} as AddTodo)).to.eventually.equal(1)
+      return expect(todosService.add({} as AddTodo)).to.eventually.equal(1)
     })
   })
 
   describe('list', () => {
     it('should throw if todosModel.list throws', () => {
       sinon.stub(todosModel, 'list').rejects()
-      expect(todosService.list()).to.eventually.be.rejected
+      return expect(todosService.list()).to.eventually.be.rejected
     })
 
     it('should return result', () => {
       sinon.stub(todosModel, 'list').resolves([{ id: 1 }] as Todo[])
-      expect(todosService.list()).to.eventually.deep.equal([{ id: 1 }])
+      return expect(todosService.list()).to.eventually.deep.equal([{ id: 1 }])
     })
   })
 })
