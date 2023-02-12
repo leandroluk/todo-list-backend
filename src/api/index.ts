4305f090c0981ed40538bf31304b553f989729b9
Leandro Santiago Gomes <leandroluk@gmail.com>
2023-02-11 21:22:08 -0300

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,14 +7,15 @@ import routes from './routes'
 
 const api = express()
 
+// input middlewares
 api.use(express.json())
 api.use(express.urlencoded({ extended: true }))
 api.use(compression({ threshold: 0 }))
 api.use(helmet())
 api.use(cors)
-
+// root router
 api.use('/', routes)
-
+// output middlewares
 api.use(errorHandler)
 
 export default api
